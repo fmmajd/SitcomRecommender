fe760f736643818c88063a1b29cfb33f2d7b5fbf
fmmajd <fmmajd452@gmail.com>
2016-10-08 21:44:04 +0330

    database created

--- models.py
+++ models.py
@@ -1,4 +1,18 @@
-class Episode:
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Episode(Base):
+
+    __tablename__ = 'episodes'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    index = Column(Integer)
+    season = Column(Integer)
+    sitcome_id = Column(Integer, ForeignKey('sitcoms.id'))
+
 
     def __init__(self, season, index, title):
         self.season = season
@@ -7,7 +21,11 @@ class Episode:
         self.isSeen = False
 
 
-class Sitcom:
+class Sitcom(Base):
+
+    __tablename__ = 'sitcoms'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
 
     def __init__(self, title,seasonNum):
         self.title = title
@@ -16,3 +34,8 @@ class Sitcom:
 
     def addEpisode(self, episode):
         self.__episodes.append(episode)
+
+
+engine = create_engine('sqlite:///whattowatch.db')
+
+Base.metadata.create_all(engine)
