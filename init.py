8b07e87007c6a722361ab570f807c1736de097fd
fmmajd <fmmajd452@gmail.com>
2016-10-13 22:56:43 +0330

    now can add more shows.

--- init.py
+++ init.py
@@ -1,13 +1,25 @@
+#this imports SHOULD NOT be removed
 import database.init
 import database.db
 
 import sys
+import json
 
 from database.init import initShow
 
 if __name__ == '__main__':
-    if(len(sys.argv) < 2):
-        name = 'Friends'
-    else:
-        name = sys.argv[1]
+
+    with open("shows/shows_list.json") as data_file:
+        shows = json.load(data_file)
+
+    print("\nchoose a show:")
+    for i in range(0, len(shows)):
+        print(str(i) + ": " + shows[i]['title'])
+
+    print()#spacing line
+
+    name = shows[int(input())]['file_name']
+
+    print("name: " + name)
+
     initShow(name)
